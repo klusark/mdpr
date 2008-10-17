57f13cb291460ecd6c2784be58ea763cc6dbe7b5
joelteichroeb <joelteichroeb@localhost>
2008-10-17 03:49:22 +0000

    not sure what is done but i want to get it committed.

--- src/WeaponManager.cpp
+++ src/WeaponManager.cpp
@@ -3,23 +3,44 @@ extern "C" {
 #include "lualib.h"
 #include "lauxlib.h"
 }
-
+#include "SDL/SDL_image.h"
+#include "GameManager.hpp"
 #include "WeaponManager.hpp"
 
-WeaponManager::WeaponManager()
+WeaponManager::WeaponManager(GameManager *gm)
 {
-	//lua_register( luaState, "addNPC", l_addNPC );
+	/*this->gm = gm;
+	lua_register(gm->luaState, "loadImages", &WeaponManager::loadImages);
+	
+	luaL_dofile(gm->luaState, "./weapons/gun.lua");
+	lua_getglobal(gm->luaState, "use");
+	lua_call(gm->luaState, 0, 0);
+	*/
 
-	//luaL_dofile(luaState, "./weapons/gun.lua");
 }
 
 WeaponManager::~WeaponManager()
 {
 }
 
-/*int l_addNPC( lua_State* luaVM)
+void WeaponManager::use()
 {
-   printf("%s\n",lua_tostring(luaVM, -1));
-   lua_pushnumber( luaVM, 0 );
-   return 1;
+}
+
+/*int WeaponManager::loadImages(lua_State *luaState)
+{
+	//printf("%s\n",);
+	std::string name = std::string(lua_tostring(luaState, -1));
+	SDL_RWops *rwop;
+	std::string file;
+	file += "data/main/";
+	file += name;
+	file += ".png";
+	rwop = SDL_RWFromFile(file.c_str(), "rb");
+	gm->images[name] = IMG_LoadPNG_RW(rwop);
+	if (!images[name])
+		printf("IMG_LoadPNG_RW: %s\n", IMG_GetError());
+	SDL_FreeRW(rwop);
+	flippedImages[name] = flipImage(images[name]);
+	return 1;
 }*/
