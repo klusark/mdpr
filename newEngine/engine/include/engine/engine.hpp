7f951da6e11baafca238338c230305a367015af7
joelteichroeb <joelteichroeb@localhost>
2008-10-20 20:52:07 +0000

    back to using sdl. more done

--- newEngine/engine/include/engine/engine.hpp
+++ newEngine/engine/include/engine/engine.hpp
@@ -1,6 +1,4 @@
-#define EngineLib __declspec( dllexport )
 namespace engine{
-	namespace window{
-		extern EngineLib bool createWindow(int screenWidth, int screenHeight, int bitsPerPixel, const char * title);
-	}
+	EngineLib bool initEngine();
+	EngineLib bool quit();
 }
--- newEngine/game/src/main.cpp
+++ newEngine/game/src/main.cpp
@@ -1,13 +1,21 @@
-#include "engine/engine.hpp"
+#include "engine/engineAll.hpp"
 #include "stdio.h"
 int main()
 {
-	if (!engine::window::createWindow(300, 300, 32, "test")){
+	engine::initEngine();
+
+	if (!engine::window::createWindow(300, 300, 32, "test", 0)){
 		return 0;
 	}
-	while (1){
 
+	try{
+		while (1){
+			engine::window::eventLoop();
+			engine::window::mainLoop();
+		}
+	}catch(int exception){
 	}
-	
+
+	engine::quit();
 	return 1;
 }
