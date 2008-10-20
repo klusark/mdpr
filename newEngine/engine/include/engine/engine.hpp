d5d9dc5d453ecd2c36f3006b0441033aa90c37aa
joelteichroeb <joelteichroeb@localhost>
2008-10-20 03:50:47 +0000

    a bit more

--- newEngine/engine/include/engine/engine.hpp
+++ newEngine/engine/include/engine/engine.hpp
@@ -1,4 +1,6 @@
 #define EngineLib __declspec( dllexport )
 namespace engine{
-	extern EngineLib bool createWindow(int screenWidth, int screenHeight, int bitsPerPixel, const char * title);
+	namespace window{
+		extern EngineLib bool createWindow(int screenWidth, int screenHeight, int bitsPerPixel, const char * title);
+	}
 }
--- newEngine/game/src/main.cpp
+++ newEngine/game/src/main.cpp
@@ -2,8 +2,11 @@
 #include "stdio.h"
 int main()
 {
-	if (!engine::createWindow(300, 300, 32, "test")){
+	if (!engine::window::createWindow(300, 300, 32, "test")){
 		return 0;
+	}
+	while (1){
+
 	}
 	
 	return 1;
