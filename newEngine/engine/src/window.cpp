45c9f8f045482cfd97c27793e8c6df3237b7b87a
joelteichroeb <joelteichroeb@localhost>
2008-10-22 20:51:22 +0000

    but more stuffs

--- newEngine/engine/src/window.cpp
+++ newEngine/engine/src/window.cpp
@@ -3,6 +3,7 @@
 
 namespace engine{
 	namespace window{
+		bool isActive = true;
 		EngineLib bool createWindow(int screenWidth, int screenHeight, int bitsPerPixel, const char * title, bool fullScreenFlag)
 		{
 			if (!SDL_WasInit(SDL_INIT_VIDEO)){
@@ -10,91 +11,129 @@ namespace engine{
 				return false;
 			}
 
+		    int videoFlags  = SDL_OPENGL;          /* Enable OpenGL in SDL */
+			videoFlags |= SDL_GL_DOUBLEBUFFER; /* Enable double buffering */
+			videoFlags |= SDL_HWPALETTE;       /* Store the palette in hardware */
+			videoFlags |= SDL_RESIZABLE;       /* Enable window resizing */
+
+			const SDL_VideoInfo *videoInfo = SDL_GetVideoInfo( );
+			if (videoInfo->hw_available){
+				videoFlags |= SDL_HWSURFACE;
+			}else{
+				videoFlags |= SDL_SWSURFACE;
+			}
+
+			if (videoInfo->blit_hw){
+				videoFlags |= SDL_HWACCEL;
+			}
+
 			SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1 );
 
-			if (fullScreenFlag)
-				SDL_SetVideoMode(screenWidth, screenHeight, bitsPerPixel, SDL_OPENGL | SDL_RESIZABLE | SDL_FULLSCREEN);
-			else
-				SDL_SetVideoMode(screenWidth, screenHeight, bitsPerPixel, SDL_OPENGL | SDL_RESIZABLE);
+			//if (fullScreenFlag)
+
+			SDL_SetVideoMode(screenWidth, screenHeight, bitsPerPixel, videoFlags);
+
 
 			SDL_WM_SetCaption(title, title);
 			
 
-			glEnable( GL_TEXTURE_2D );
- 
-			glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
-			 
-			glViewport( 0, 0, screenWidth, screenHeight );
-			 
-			glClear( GL_COLOR_BUFFER_BIT );
-			 
-			glMatrixMode( GL_PROJECTION );
-			glLoadIdentity();
-			 
-			glOrtho(0.0f, screenWidth, screenHeight, 0.0f, -1.0f, 1.0f);
-			 
-			glMatrixMode( GL_MODELVIEW );
-			glLoadIdentity();
+			/* Enable smooth shading */
+			glShadeModel( GL_SMOOTH );
 
+			/* Set the background black */
+			glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+
+			/* Depth buffer setup */
+			glClearDepth( 1.0f );
+
+			/* Enables Depth Testing */
+			glEnable( GL_DEPTH_TEST );
+
+			/* The Type Of Depth Test To Do */
+			glDepthFunc( GL_LEQUAL );
+
+			/* Really Nice Perspective Calculations */
+			glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
+
+			windowResize(screenWidth, screenHeight);
 			return true;
 		}
-		void window_resized(int w, int h)
-{
-	SDL_SetVideoMode(w, h, 16, SDL_OPENGL | SDL_RESIZABLE);
-			glEnable( GL_TEXTURE_2D );
- 
-			glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
-			 
-			glViewport( 0, 0, w, h );
-			 
-			glClear( GL_COLOR_BUFFER_BIT );
-			 
-			glMatrixMode( GL_PROJECTION );
-			glLoadIdentity();
-			 
-			glOrtho(0.0f, w, h, 0.0f, -1.0f, 1.0f);
-			 
-			glMatrixMode( GL_MODELVIEW );
-			glLoadIdentity();
-}
 
-		EngineLib void eventLoop()
+		void windowResize(int width, int height)
 		{
-			SDL_Event events;
-			while (SDL_PollEvent(&events)){
-				if (events.type == SDL_QUIT){
-					//quit the game
-					throw 1; 
-				}else if (events.type == SDL_VIDEORESIZE){
-					window_resized(events.resize.w, events.resize.h);
-				}
+			/* Height / width ration */
+			GLfloat ratio;
+
+			/* Protect against a divide by zero */
+			if ( height == 1 )
+				height = 100;
+
+			ratio = ( GLfloat )width / ( GLfloat )height;
+
+			/* Setup our viewport. */
+			glViewport( 0, 0, ( GLsizei )width, ( GLsizei )height );
+
+			/* change to the projection matrix and set our viewing volume. */
+			glMatrixMode( GL_PROJECTION );
+			glLoadIdentity( );
+
+			/* Set our perspective */
+			gluPerspective( 45.0f, ratio, 0.1f, 100.0f );
+
+			/* Make sure we're chaning the model view and not the projection */
+			glMatrixMode( GL_MODELVIEW );
+
+			/* Reset The View */
+			glLoadIdentity( );
+		}
+
+		void draw()
+		{
+			/* These are to calculate our fps */
+			static GLint T0     = 0;
+			static GLint Frames = 0;
+
+			/* Clear The Screen And The Depth Buffer */
+			glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+			/* Move Left 1.5 Units And Into The Screen 6.0 */
+			glLoadIdentity();
+			/*glTranslatef( 0.0f, 0.0f, -3.0f );
+
+			glBegin( GL_TRIANGLES );             // Drawing Using Triangles       
+			  glVertex3f( -1.1f,  1.0f,  0.0f ); // Top Of Triangle               
+			  glVertex3f( -1.0f, -1.0f,  0.0f ); // Left Of Triangle              
+			  glVertex3f(  1.0f, -1.0f,  0.0f ); // Right Of Triangle             
+			glEnd( );                            // Finished Drawing The Triangle 
+			*/
+			// Move Right 3 Units 
+			glTranslatef( 0.0f, 0.0f, -3.0f );
+
+			// Set The Color To Blue One Time Only 
+			glColor3f( 0.5f, 1.0f, 1.0f);
+			
+			glBegin( GL_QUADS );                 /* Draw A Quad              */
+			  glVertex3f(  1.25f,  1.25f,  0.0f ); /* Top Right Of The Quad    */
+			  glVertex3f( -1.25f,  1.25f,  0.0f ); /* Top Left Of The Quad     */
+			  glVertex3f( -1.25f, -1.25f,  0.0f ); /* Bottom Left Of The Quad  */
+			  glVertex3f(  1.25f, -1.25f,  0.0f ); /* Bottom Right Of The Quad */
+			glEnd( );                            /* Done Drawing The Quad    */
+
+			/* Draw it to the screen */
+			SDL_GL_SwapBuffers( );
+
+			/* Gather our frames per second */
+			Frames++;
+			
+			GLint t = SDL_GetTicks();
+			if (t - T0 >= 5000) {
+				GLfloat seconds = (t - T0) / 1000.0;
+				GLfloat fps = Frames / seconds;
+				printf("%d frames in %g seconds = %g FPS\n", Frames, seconds, fps);
+				T0 = t;
+				Frames = 0;
+			
 			}
 		}
-
-		EngineLib void mainLoop()
-		{
-			glBegin( GL_QUADS );
-				//Top-left vertex (corner)
-				glTexCoord2i( 0, 0 );
-				glVertex3f( 100, 100, 0.0f );
-			 
-				//Bottom-left vertex (corner)
-				glTexCoord2i( 1, 0 );
-				glVertex3f( 228, 100, 0 );
-			 
-				//Bottom-right vertex (corner)
-				glTexCoord2i( 1, 1 );
-				glVertex3f( 228, 228, 0 );
-			 
-				//Top-right vertex (corner)
-				glTexCoord2i( 0, 1 );
-				glVertex3f( 100, 228, 0 );
-			glEnd();// Done Drawing The Quad
-			SDL_GL_SwapBuffers();
-		}
-		
-
-
-		
 	}
 }
