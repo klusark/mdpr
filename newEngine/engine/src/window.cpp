c18856b5d6fe06a4b0ceea5c245bda475ca59188
joelteichroeb <joelteichroeb@localhost>
2008-10-27 20:51:39 +0000

    More 3d drawing

--- newEngine/engine/src/window.cpp
+++ newEngine/engine/src/window.cpp
@@ -4,14 +4,21 @@
 namespace engine{
 	namespace window{
 		bool isActive = true;
+		int videoFlags;
+		int screenWidth;
+		int screenHeight;
+		int bitsPerPixel;
 		EngineLib bool createWindow(int screenWidth, int screenHeight, int bitsPerPixel, const char * title, bool fullScreenFlag)
 		{
+			window::screenWidth = screenWidth;
+			window::screenHeight = screenHeight;
+			window::bitsPerPixel = bitsPerPixel;
 			if (!SDL_WasInit(SDL_INIT_VIDEO)){
 				printf("SDL not initializeed\n");
 				return false;
 			}
 
-		    int videoFlags  = SDL_OPENGL;          /* Enable OpenGL in SDL */
+		    videoFlags  = SDL_OPENGL;          /* Enable OpenGL in SDL */
 			videoFlags |= SDL_GL_DOUBLEBUFFER; /* Enable double buffering */
 			videoFlags |= SDL_HWPALETTE;       /* Store the palette in hardware */
 			videoFlags |= SDL_RESIZABLE;       /* Enable window resizing */
@@ -29,31 +36,12 @@ namespace engine{
 
 			SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1 );
 
-			//if (fullScreenFlag)
+			if (fullScreenFlag){
+			}
 
-			SDL_SetVideoMode(screenWidth, screenHeight, bitsPerPixel, videoFlags);
-
+			//SDL_SetVideoMode(screenWidth, screenHeight, bitsPerPixel, videoFlags);
 
 			SDL_WM_SetCaption(title, title);
-			
-
-			/* Enable smooth shading */
-			glShadeModel( GL_SMOOTH );
-
-			/* Set the background black */
-			glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
-
-			/* Depth buffer setup */
-			glClearDepth( 1.0f );
-
-			/* Enables Depth Testing */
-			glEnable( GL_DEPTH_TEST );
-
-			/* The Type Of Depth Test To Do */
-			glDepthFunc( GL_LEQUAL );
-
-			/* Really Nice Perspective Calculations */
-			glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
 
 			windowResize(screenWidth, screenHeight);
 			return true;
@@ -61,30 +49,15 @@ namespace engine{
 
 		void windowResize(int width, int height)
 		{
-			/* Height / width ration */
-			GLfloat ratio;
+			SDL_SetVideoMode(width, height, bitsPerPixel, videoFlags);
+			glMatrixMode (GL_PROJECTION);
+			glLoadIdentity ();
+			glOrtho (0, 320, 200, 0, 0, 1);
+			glMatrixMode (GL_MODELVIEW);
 
-			/* Protect against a divide by zero */
-			if ( height == 1 )
-				height = 100;
-
-			ratio = ( GLfloat )width / ( GLfloat )height;
-
-			/* Setup our viewport. */
-			glViewport( 0, 0, ( GLsizei )width, ( GLsizei )height );
-
-			/* change to the projection matrix and set our viewing volume. */
-			glMatrixMode( GL_PROJECTION );
-			glLoadIdentity( );
-
-			/* Set our perspective */
-			gluPerspective( 45.0f, ratio, 0.1f, 100.0f );
-
-			/* Make sure we're chaning the model view and not the projection */
-			glMatrixMode( GL_MODELVIEW );
-
-			/* Reset The View */
-			glLoadIdentity( );
+			glPushMatrix();
+			glLoadIdentity();
+			
 		}
 
 		void draw()
@@ -93,31 +66,22 @@ namespace engine{
 			static GLint T0     = 0;
 			static GLint Frames = 0;
 
-			/* Clear The Screen And The Depth Buffer */
-			glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-
-			/* Move Left 1.5 Units And Into The Screen 6.0 */
+		  
+			//glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+		  
 			glLoadIdentity();
-			/*glTranslatef( 0.0f, 0.0f, -3.0f );
-
-			glBegin( GL_TRIANGLES );             // Drawing Using Triangles       
-			  glVertex3f( -1.1f,  1.0f,  0.0f ); // Top Of Triangle               
-			  glVertex3f( -1.0f, -1.0f,  0.0f ); // Left Of Triangle              
-			  glVertex3f(  1.0f, -1.0f,  0.0f ); // Right Of Triangle             
-			glEnd( );                            // Finished Drawing The Triangle 
-			*/
-			// Move Right 3 Units 
-			glTranslatef( 0.0f, 0.0f, -3.0f );
-
-			// Set The Color To Blue One Time Only 
-			glColor3f( 0.5f, 1.0f, 1.0f);
+		  
 			
-			glBegin( GL_QUADS );                 /* Draw A Quad              */
-			  glVertex3f(  1.25f,  1.25f,  0.0f ); /* Top Right Of The Quad    */
-			  glVertex3f( -1.25f,  1.25f,  0.0f ); /* Top Left Of The Quad     */
-			  glVertex3f( -1.25f, -1.25f,  0.0f ); /* Bottom Left Of The Quad  */
-			  glVertex3f(  1.25f, -1.25f,  0.0f ); /* Bottom Right Of The Quad */
-			glEnd( );                            /* Done Drawing The Quad    */
+			glBegin(GL_TRIANGLES);
+				glColor3ub(255, 0, 0);
+				glVertex2d(0, 0);
+			  
+				glColor3ub(0, 255, 0);
+				glVertex2d(100,0);
+			  
+				glColor3ub(0, 0, 255);
+				glVertex2d(50, 50);
+			glEnd();
 
 			/* Draw it to the screen */
 			SDL_GL_SwapBuffers( );
@@ -127,7 +91,7 @@ namespace engine{
 			
 			GLint t = SDL_GetTicks();
 			if (t - T0 >= 5000) {
-				GLfloat seconds = (t - T0) / 1000.0;
+				GLfloat seconds = (t - T0) / 1000.0f;
 				GLfloat fps = Frames / seconds;
 				printf("%d frames in %g seconds = %g FPS\n", Frames, seconds, fps);
 				T0 = t;
