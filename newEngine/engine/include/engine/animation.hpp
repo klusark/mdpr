65f7f66aceea327b1ae743ebfb5d396260ddf802
joelteichroeb <joelteichroeb@localhost>
2008-11-02 17:21:56 +0000

    animations work

--- newEngine/engine/include/engine/animation.hpp
+++ newEngine/engine/include/engine/animation.hpp
@@ -18,13 +18,15 @@ namespace engine
 			unsigned int frames[8];
 			/// the current frame
 			unsigned short currentFrame;
-		};
+		} *currentAnimation;
+		unsigned int lastAnimationTime;
 		std::map<std::string, animationInfo *> Animations;
 	public:
 		EngineLib Animation();
 		virtual EngineLib ~Animation();
 		virtual EngineLib void update();
 		void EngineLib makeAnimation(const char *name, unsigned short numFrames, unsigned short delay, const char *textures, ...);
+		void EngineLib changeAnimation(const char *name);
 	};
 }
 #endif
