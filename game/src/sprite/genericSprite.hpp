e14f692a9fa6584c2949423aa6219a01c1e9d109
joelteichroeb <joelteichroeb@localhost>
2009-06-05 09:24:29 -0700

    Added comments to generic sprite

--- game/src/sprite/genericSprite.hpp
+++ game/src/sprite/genericSprite.hpp
@@ -12,10 +12,23 @@
 
 class genericPowerUp;
 
+//! The base sprite class
+/*!
+All sprites must inherit this class
+ Provides movement, animation, and  access to colliiton
+*/
 class genericSprite : public sf::Sprite
 {
 public:
+	//! The constructor.
+	/*!
+	Constructs the generic sprite
+	\param name Name of the sprite
+	\param spriteType Name of the type of sprite. Used for loading the sprites details from a file. 
+	\param Image  The static image from the sprite class that inherits this one. 
+	*/
 	genericSprite(const std::string &name, std::string spriteType, sf::Image &Image);
+	//!The deconstructor
 	~genericSprite();
 
 	float getXAccel();
@@ -28,36 +41,77 @@ public:
 	void setXVelocity(float xVelocity);
 	void setYVelocity(float yVelocity);
 
+	//!Updates the sprite
+	/*!
+	Can be reimplemented in an inheritor for more updating control
+	*/
 	virtual void update();
+	
+	//!Draws the sprite to the screen
+	/*!
+	Can be reimplemented in an inheritor for more drawing control
+	\param App The render window to draw the sprite onto. 
+	*/
 	virtual void draw(sf::RenderWindow &App);
 
+	//!Changes the current animation of the sprite
+	/*!
+	\param name name of the sprite that has already been converted into a unsigned int.
+	*/
 	void changeAnimation(unsigned int name);
+	
+	//!Changes the current animation of the sprite
+	/*!
+	\param name in name of the sprite in string form
+	*/
 	void changeAnimation(std::string name);
 
 	void kill();
 
 	virtual void die();
 
+	//!The name of the sprite
 	std::string name;
 
+	//!pointer to the current powerup
 	boost::shared_ptr<genericPowerUp> currentPowerup;
-	bool hasPowerUp;
-	bool onGround;
-	bool inputDisabled;
-	bool nonNetworked;
-	bool keyLock;
-	bool animationLock;
-	bool noAnimation;
+	
+	//!True if the sprite currently holds a powerup
+	bool hasPowerUp,
+	
+		//!True if the sprite is currently on the groud.
+		onGround, 
+		
+		//!True if the sprite has its input disabled
+		inputDisabled, 
+		
+		//!True if the sprite should be server side only.
+		nonNetworked, 
+		
+		//TODO figure out the difference between this and inputDisabled
+		//!I am not sure...
+		keyLock, 
+		
+		//!true if the sprite should be locked to it's current animaiton
+		animationLock, 
+		
+		//!ture if the sprite does not have any animation
+		noAnimation,
+		
+		//!true if the sprite is currently flipped.
+		flipped;
 
+	//!The name of the last animation. Used to figure out when to send animation change packets.
 	std::string lastAnimationName;
 
+	//!The sprite type. Mainly used for collision detection
 	spriteTypes spriteType;
 
+	//!Pointer to the current animaiton
 	boost::shared_ptr<Animation> currentAnimation;
 	
 	float lastX, lastY;
 	int timesSkiped;
-	bool flipped;
 	spriteStates currentState;
 protected:
 	sf::Image &Image;
@@ -70,4 +124,4 @@ protected:
 	animationContainer Animations;
 };
 
-#endif // ifndef genericSprite_hpp
+#endif // #ifndef genericSprite_hpp
