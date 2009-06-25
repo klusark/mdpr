0972d6f9293a6865a915f4d5cc3b407d0310bf4a
joelteichroeb <joelteichroeb@localhost>
2009-06-25 16:48:24 -0700

    Modified the master server for better shutdown

--- masterServer/src/main.cpp
+++ masterServer/src/main.cpp
@@ -1,9 +1,21 @@
 #include <boost/shared_ptr.hpp>
 #include "networkMasterServer.hpp"
+#include <signal.h>
+
+boost::shared_ptr<networkMasterServer> masterServer;
+
+void signalHandler(int sig)
+{
+	masterServer->stopServer();
+}
 
 int main()
 {
 	masterServer = boost::shared_ptr<networkMasterServer>(new networkMasterServer());
-	masterServer->runServer();
 
+	signal(SIGABRT, &signalHandler);
+	signal(SIGTERM, &signalHandler);
+	signal(SIGINT, &signalHandler);
+
+	masterServer->runServer();
 }
